6c1b6dab5c254b78d3994a3b2bfaae309945775b
Eejit <76887639+Eejit43@users.noreply.github.com>
2025-01-15 22:12:31 -0500

    feat: implement script functionality

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 #! /usr/bin/env bun
 
+import { createCanvas, registerFont } from 'canvas';
+import { type Declaration, parse as parseCss } from 'css';
+import { existsSync } from 'node:fs';
+import path from 'node:path';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
@@ -13,4 +17,75 @@ const argv = await yargs(hideBin(process.argv))
     .version(false)
     .parse();
 
-console.log(argv);
+const cssFile = Bun.file(argv.css);
+
+let cssContent: string;
+
+try {
+    cssContent = await cssFile.text();
+} catch {
+    console.error('Nonexistent CSS file provided!');
+
+    process.exit(1);
+}
+
+if (!(await Bun.file(argv.ttf).exists())) {
+    console.error('Nonexistent TTF file provided!');
+
+    process.exit(1);
+}
+
+if (!existsSync(argv.outdir)) {
+    console.error('Nonexistent output directory provided!');
+
+    process.exit(1);
+}
+
+const parsedCss = parseCss(cssContent);
+
+let foundIconCode: string | undefined;
+
+for (const rule of parsedCss.stylesheet!.rules) {
+    if (rule.type !== 'rule') continue;
+
+    const hasIconCode = rule.selectors?.some((selector) => selector === `.fa-${argv.icon}`);
+
+    if (!hasIconCode) continue;
+
+    const iconCode = rule.declarations?.find((declaration) => declaration.type === 'declaration' && declaration.property === '--fa') as
+        | Declaration
+        | undefined;
+
+    if (iconCode) {
+        foundIconCode = iconCode.value?.replaceAll(/\\|"/g, '');
+
+        break;
+    }
+}
+
+if (!foundIconCode) {
+    console.error('Nonexistent icon provided!');
+
+    process.exit(1);
+}
+
+registerFont(argv.ttf, { family: 'Font Awesome' });
+
+const iconSize = 200;
+
+const canvas = createCanvas(iconSize, iconSize);
+const context = canvas.getContext('2d');
+
+context.font = `${iconSize}px "Font Awesome"`;
+
+const foundIconCodeString = String.fromCodePoint(Number.parseInt(foundIconCode, 16));
+
+const iconMeasurements = context.measureText(foundIconCodeString);
+
+context.fillText(foundIconCodeString, 0, iconMeasurements.actualBoundingBoxAscent);
+
+const pngBuffer = canvas.toBuffer();
+
+const outputFile = Bun.file(path.join(argv.outdir, `${argv.icon}.png`));
+
+void outputFile.write(pngBuffer);
